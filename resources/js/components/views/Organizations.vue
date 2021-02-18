8cb3788a4e9f27fe9bd933e89296f53ebc2e0f51
Shane Poppleton <shane@bjja.com.au>
2021-02-19 06:26:29 +1000

    Work for 18/02/21

--- resources/js/components/views/Organizations.vue
+++ resources/js/components/views/Organizations.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-col flex-1 focus:outline-none" tabindex="0">
         <action-bar :search="true" placeholder="Search organizations" :action="action"></action-bar>
-        <main class="flex flex-col flex-1 relative pb-8 ">
+        <main class="flex flex-col flex-1 relative overflow-hidden">
             <page-header @action="createOrganizationDialog" :action-enabled="true"
                          action-label="Create Organization"></page-header>
             <div v-if="! organizationId" class="mt-8">
@@ -10,21 +10,39 @@
                 </h2>
                 <organization-list @open-organization="openOrganization"/>
             </div>
-            <div v-else class="flex flex-col max-w-6xl mx-auto mt-8 px-4 sm:px-6">
+            <div v-else class="flex flex-col max-w-6xl mx-auto p-6 sm:px-6 overflow-hidden">
                 <template v-if="organization">
-                    <div class="pb-5 border-b border-gray-200">
-                        <h3 class="text-lg leading-6 font-medium text-gray-900" v-text="organization.name" />
+
+                    <div class="pb-5 border-b border-gray-200"> <!-- Organization Name -->
+                        <h3 class="text-lg leading-6 font-medium text-gray-900" v-text="organization.name"/>
                     </div>
-                    <div class="flex-1 mt-2">
-                        <label for="country" class="block text-sm font-medium text-gray-700">
+
+                    <div class="mt-2"> <!-- Head office selection -->
+                        <label for="headOffice" class="block text-sm font-medium text-gray-700">
                             Head Office
                         </label>
                         <div class="mt-1">
-                            <select id="country" name="country" autocomplete="country" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block sm:text-sm border-gray-300 rounded-md">
-                                <option v-for="company in companiesByOrganization(organizationId)" :value="company.id">{{company.name}}</option>
+                            <select id="headOffice" name="headOffice"
+                                    class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block sm:text-sm border-gray-300 rounded-md">
+                                <option v-for="company in companiesByOrganization(organizationId)" :value="company.id">
+                                    {{ company.name }}
+                                </option>
                             </select>
                         </div>
+                    </div>
+                    <div class="mt-4 flex flex-col flex-1 overflow-hidden">
+                        <label class="block text-sm font-medium text-gray-700">
+                            Notes
+                        </label>
                         <organization-notes :organization="organization"></organization-notes>
+                    </div>
+
+
+                    <div class="mt-4">
+                        <button data-v-7fb418a7="" type="button"
+                                class="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-cyan-600 hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500">
+                            Add Note
+                        </button>
                     </div>
                 </template>
             </div>
@@ -33,7 +51,7 @@
     </div>
 </template>
 <script>
-import { mapGetters } from 'vuex';
+import {mapGetters} from 'vuex';
 
 export default {
     data() {
@@ -49,7 +67,7 @@ export default {
             return null;
         },
         organization() {
-            return this.organizations.find(x => x.id===+this.organizationId);
+            return this.organizations.find(x => x.id === +this.organizationId);
         },
 
     },
